c988def791bb3e160709be361da9edfddd48f8ce
Faith Mutanu <faithtanuh@gmail.com>
2021-06-22 05:16:03 -0700

    Functions

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,24 +1,22 @@
 #include "holberton.h"
 
 /**
- * main - entry point
- *
- * Description: Prints Holberton with _putchar
- *
- * Return: always 0
+ * main-prints Holberton, followed by a new line
+ * Return: always 0(Success)
  */
 
 int main(void)
 {
-	int i = 0;
-	char c;
-	char s[] = "Holberton\n";
-
-	while (i <= 9)
-	{
-		c = s[i];
-		_putchar(c);
-		i++;
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	
 	return (0);
 }
