c5af1138d739ef9f33ffdf8dd262e26a953ff0ee
Faith Mutanu <85233099+MsMutanu@users.noreply.github.com>
2021-06-22 18:03:44 +0300

    Functions

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,16 +7,12 @@
 
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
-	
+	char a[]={'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', '\n'}
+	char b;
+	for (; i < 10; i++)
+	{
+		b= a[i];
+		_putchar(b);
+	}	
 	return (0);
 }
