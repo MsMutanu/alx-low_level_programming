84c4317642ec3711110353c6ffec6d3315a9f6bc
Faith Mutanu <faithtanuh@gmail.com>
2021-06-24 05:41:28 -0700

    More Functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+
+/**
+ * _isupper - ichecks for uppercase character
+ * returns 1 (Succes))
+ */
+int _isuppercase(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,9 @@
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+
+
+#endif
