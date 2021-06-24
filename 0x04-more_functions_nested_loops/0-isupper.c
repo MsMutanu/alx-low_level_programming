bf2efaf15bc685c8c2fd3107fe189efe51788235
Faith Mutanu <faithtanuh@gmail.com>
2021-06-24 07:43:40 -0700

    More functions

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 
 /**
- * _isupper - ichecks for uppercase character
- * returns 1 (Succes))
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
  */
-int _isupper(int c)
+int _isupper(int c);
 {
 	return (c >= 'A' && c <= 'Z');
 }
