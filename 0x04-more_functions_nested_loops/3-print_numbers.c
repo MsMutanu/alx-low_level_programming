9ce1300a53e765cc95dffa9c7c44ac0a8b3429e8
Faith Mutanu <faithtanuh@gmail.com>
2021-06-24 09:11:30 -0700

    More functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9 followed by a new line
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar(a + '0');
+	}
+	_putchar('\n');
+}
