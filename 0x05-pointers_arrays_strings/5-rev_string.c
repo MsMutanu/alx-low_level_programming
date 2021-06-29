6c3a0456e7cf0bf4e171a35f1ef9e27d5934f083
Faith Mutanu <faithtanuh@gmail.com>
2021-06-29 10:09:28 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: a pointer
+ */
+
+void rev_string(char *s)
+{
+	int i, j, len;
+	
+	i = 0;
+	
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	len = i;
+
+	for (j = len - 1 ; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
