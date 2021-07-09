af1f540c16f36858a7b85f2d7d5016dee484159a
Faith Mutanu <faithtanuh@gmail.com>
2021-07-09 05:48:15 -0700

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - returns the length of a sring
+ * @s: a pointer
+ * Return: lenght of string
+ */
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (*s != 0)
+	{
+		count++;
+		_strlen_recursion(++s);
+	}
+	else
+	{
+		return (count);
+	}
+}
