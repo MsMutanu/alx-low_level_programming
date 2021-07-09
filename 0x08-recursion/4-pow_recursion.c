984767c8a56621367445afcc87af9b24789ad37d
Faith Mutanu <faithtanuh@gmail.com>
2021-07-09 07:35:09 -0700

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - Returns the value of x raised to power of y
+ * @x: a number
+ * @y: the power
+ * Return: the value of x raised to power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (-1);
+}
