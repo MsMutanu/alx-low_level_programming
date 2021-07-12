89accc8e90a6c1eb160d8ed4d96e1a197a62693f
Faith Mutanu <faithtanuh@gmail.com>
2021-07-12 10:25:57 -0700

    arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: the number of arguments passed
+ * @argv: array of pointers to the string of arguments passed to the command
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
