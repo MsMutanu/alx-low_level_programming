4f512b438fce6373641c48f8ec16ce3e84161811
Faith Mutanu <faithtanuh@gmail.com>
2021-07-12 07:36:27 -0700

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name followed by a new line
+ * @argc: the number of arguments passed
+ * @argv: array of pointers to the string of arguments passed to the command
+ * Return: 0 if succesful
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
