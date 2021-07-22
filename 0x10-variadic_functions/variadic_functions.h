cd08eb037c76ed58b6c96bec283b8e6b7fd1abf2
Faith Mutanu <faithtanuh@gmail.com>
2021-07-22 13:41:33 -0700

    variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,8 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef VARIADIC_H
+#define VARIADIC_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_then_all(const unsigned int n, ...);
