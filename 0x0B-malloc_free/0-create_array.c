07b987c4a7339d20056f1544e0b2cc814a5d5042
Faith Mutanu <faithtanuh@gmail.com>
2021-07-24 07:02:18 -0700

    Malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	arr = malloc(size_of(char) * size);
+	arr = malloc(sizeof(char) * size);
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
