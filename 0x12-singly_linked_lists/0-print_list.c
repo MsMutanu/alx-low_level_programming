0676860275dbd3f5e6b9e33c0efeedd657c7e726
Faith Mutanu <faithtanuh@gmail.com>
2021-08-09 04:59:33 -0700

    Linked Lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - prints the elements of a list list_t
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+	
+
+	while(h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			count++;
+			h = h->next;
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			count++;
+			h = h->next;
+		}
+	}
+	return (count);
+}
